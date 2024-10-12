c1dd2b4ad89661a9e4f0c8ca1da3bdc04c62896e
Nimira43 <n.rabone72@aol.com>
2024-10-12 17:58:40 +0100

    Starting work on Navbar component and brought in project logo

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export function Navbar() {
+  return (
+    <div className='flex py-5 items-center justify-between'>
+      <Link href='/'>
+        <Image />
+      </Link>
+    </div>
+  )
+}

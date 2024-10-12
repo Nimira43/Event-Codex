e9a0973d13048358f8e3c059dfff86899ac2e334
Nimira43 <n.rabone72@aol.com>
2024-10-12 18:05:36 +0100

    Brought in image assets

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export function Navbar() {
   return (
     <div className='flex py-5 items-center justify-between'>
       <Link href='/'>
-        <Image />
+        <Image src={}/>
       </Link>
     </div>
   )

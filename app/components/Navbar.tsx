88e60057f91e583da0ff2dfd986bd652878bbe77
Nimira43 <n.rabone72@aol.com>
2024-10-12 20:58:05 +0100

    Updated

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,6 +4,7 @@ import Logo from '@/public/logo.png'
 import { AuthModal } from './AuthModal'
 
 export function Navbar() {
+  
   return (
     <div className='flex py-5 items-center justify-between'>
       <Link href='/' className='flex items-center gap-2'>

e0fc3e3adc7da8cb3a015a728a1ed2900bfd80c2
Nimira43 <n.rabone72@aol.com>
2024-10-13 10:28:11 +0100

    In Submit created two states of button - one for Google and one for GitHub

--- app/components/SubmitButtons.tsx
+++ app/components/SubmitButtons.tsx
@@ -1,5 +1,24 @@
+import { Button } from '@/components/ui/button'
 import { useFormStatus } from 'react-dom'
+import GoogleLogo from '@/public/google.svg'
+import GitHubLogo from '@/public/github.svg'
+import Image from 'next/image'
 
 export function GoogleAuthButton() {
-  const {} = useFormStatus
+  const {pending} = useFormStatus()
+  return (
+    <>
+    {pending ? (
+      <Button>
+        <Image src={GitHubLogo} alt='Google Logo' className='size-4 mr-2' />
+        Sign in with GitHub
+      </Button>
+    ): (
+      <Button>
+        <Image src={GoogleLogo} alt='Google Logo' className='size-4 mr-2' />
+        Sign in with Google
+      </Button>
+    )}
+    </>
+  )
 }

baa17b8d5b9fe25db8490472ef2ca3eb862d4241
Nimira43 <n.rabone72@aol.com>
2024-10-13 11:54:23 +0100

    Set up Dashboard interface with the menu links and routes. Using Lucide Icons also

--- app/components/DashboardLinks.tsx
+++ app/components/DashboardLinks.tsx
@@ -1,3 +1,39 @@
+import { CalendarCheck, HomeIcon, Settings2, Users } from "lucide-react"
+
+interface iAppProps {
+  id: number
+  name: string
+  href: string
+  icon: any
+}
+
+export const dashboardLinks: iAppProps[] = [
+  {
+    id: 0,
+    name: 'Event Types',
+    href: '/dashboard',
+    icon: HomeIcon,
+  },
+  {
+    id: 1,
+    name: 'Meeting',
+    href: '/dashboard/meetings',
+    icon: Users,
+  },
+  {
+    id: 2,
+    name: 'Availability',
+    href: '/dashboard/availability',
+    icon: CalendarCheck,
+  },
+  {
+    id: 3,
+    name: 'Settings',
+    href: '/dashboard/settings',
+    icon: Settings2,
+  },
+]
+
 export function DashboardLinks() {
-  
+
 }

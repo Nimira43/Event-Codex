aff19ae1091054109174b4a6374a95702d46859a
Nimira43 <n.rabone72@aol.com>
2024-10-13 11:38:40 +0100

    Using grid for Dashboard layout and styling the sidebar first

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,8 +1,27 @@
-export default function DashboardLayout() {
+import Image from 'next/image'
+import Link from 'next/link'
+import { ReactNode } from 'react'
+import Logo from '@/public/logo.png'
+
+export default function DashboardLayout({children}: {children: ReactNode}) {
 
   return (
-    <div>
-      <h1>Dashboard Manager</h1>
-    </div>
+    <>
+      <div className='min-h-screen w-full grid md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]'>
+        <div className='hidden md:block border-r border-[#daa520] bg-[#eee]/40 '>
+          <div className='flex h-full max-h-screen flex-col gap-2'>
+            <div className='flex h-14 items-center border-b border-[#daa520] px-4 lg:[60px] lg:px-6'>
+              <Link href='/' className='flex items-center gap-2'>
+                <Image src={Logo} alt='Logo' className='size-8'/>
+                <p className='text-xl font-semibold '>
+                  Event
+                  <span className='text-[#ffa500]'> Codex</span>
+                </p>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }

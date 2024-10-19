818cf87f265a62d4e90daf7c4a43decea143bc03
Nimira43 <n.rabone72@aol.com>
2024-10-19 18:29:11 +0100

    Completed Light / Dark mode

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,13 +6,14 @@ import { DashboardLinks } from '../components/DashboardLinks'
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 import { Button } from '@/components/ui/button'
 import { Menu } from 'lucide-react'
+import { ThemeToggle } from '../components/ThemeToggle'
 
 export default function DashboardLayout({children}: {children: ReactNode}) {
 
   return (
     <>
       <div className='min-h-screen w-full grid md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]'>
-        <div className='hidden md:block border-r border-[#daa520] bg-[#eee]/40 '>
+        <div className='hidden md:block border-r border-[#daa520] bg-[#fefefe]/40 '>
           <div className='flex h-full max-h-screen flex-col gap-2'>
             <div className='flex h-14 items-center border-b border-[#daa520] px-4 lg:[60px] lg:px-6'>
               <Link href='/' className='flex items-center gap-2'>
@@ -31,7 +32,7 @@ export default function DashboardLayout({children}: {children: ReactNode}) {
           </div>
         </div>
         <div className='flex flex-col'>
-          <header className='flex h-14 items-center gap-4 border-b border-[#daa520] bg-[#eee]/40 px-3 lg:h-[60px] lg:px-6'>
+          <header className='flex h-14 items-center gap-4 border-b border-[#daa520] bg-[#fefefe]/40 px-3 lg:h-[60px] lg:px-6'>
             <Sheet>
               <SheetTrigger asChild>
                 <Button 
@@ -42,14 +43,14 @@ export default function DashboardLayout({children}: {children: ReactNode}) {
                   <Menu className='size-5'/>                
                 </Button>
               </SheetTrigger>
-              <SheetContent side='left' className='flex flex-col bg-[#eee]'>
+              <SheetContent side='left' className='flex flex-col bg-[#fefefe]'>
                 <nav className='grid gap-2 mt-10'>
                   <DashboardLinks />
                 </nav>
               </SheetContent>              
             </Sheet>
             <div className='ml-auto flex items-center gap-x-6'>
-
+              <ThemeToggle />
             </div>
           </header>
         </div>
